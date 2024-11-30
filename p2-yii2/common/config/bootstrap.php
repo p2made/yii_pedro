a31f43cd5643450aafd5372a99610aac92061182
p2made <pedrofp@me.com>
2024-12-01 00:28:14 +1000

    ding

--- p2-yii2/common/config/bootstrap.php
+++ p2-yii2/common/config/bootstrap.php
@@ -1,5 +1,9 @@
 <?php
-Yii::setAlias('@common', dirname(__DIR__));
+/**
+ * app/common/config/bootstrap.php
+ */
+Yii::setAlias('@common',   dirname(__DIR__));
+Yii::setAlias('@backend',  dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
-Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
-Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+Yii::setAlias('@console',  dirname(dirname(__DIR__)) . '/console');
+Yii::setAlias('@static',   dirname(dirname(__DIR__)) . '/static');
--- p2-yii2/common/config/main.php
+++ p2-yii2/common/config/main.php
@@ -1,11 +1,61 @@
 <?php
 return [
+	'charset' => 'utf-8',
 	'aliases' => [
 		'@bower' => '@vendor/bower-asset',
 		'@npm'   => '@vendor/npm-asset',
 	],
 	'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
 	'components' => [
+		'assetManager' => [
+			'basePath' => '@webroot/assets',
+			'baseUrl' => '@web/assets',
+			//'appendTimestamp' => true, // useful while developing custom assets
+			'bundles' => [
+				'yii\bootstrap5\BootstrapAsset' => [
+					'sourcePath' => null,
+					'baseUrl' => '//cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/',
+					'css' => [
+						'css/bootstrap.min.css',
+					],
+					'cssOptions' => [
+						'integrity' => 'sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH',
+						'crossorigin' => 'anonymous',
+					],
+				],
+				'yii\bootstrap5\BootstrapPluginAsset' => [
+					'sourcePath' => null,
+					'baseUrl' => '//cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/',
+					'js' => [
+						'js/bootstrap.bundle.min.js',
+					],
+					'jsOptions' => [
+						'integrity' => 'sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz',
+						'crossorigin' => 'anonymous',
+					],
+				],
+				'yii\web\JqueryAsset' => [
+					'sourcePath' => null,
+					'baseUrl' => '//code.jquery.com/',
+					'js' => [
+						'jquery-3.7.1.min.js',
+					],
+					'jsOptions' => [
+						'integrity' => 'sha384-1H217gwSVyLSIfaLxHbE7dRb3v4mYCKbpQvzx0cegeju1MVsGrX5xXxAvs/HgeFs',
+						'crossorigin' => 'anonymous',
+					],
+				],
+				//'yii\jui\JuiAsset' => [
+				//	'sourcePath' => null, 'css' => [], 'js' => [],
+				//],
+			],
+		],
+		'urlManager' => [
+			'class' => 'yii\web\UrlManager',
+			'enablePrettyUrl' => true,
+			'showScriptName' => false,
+			'enableStrictParsing' => false,
+		],
 		'cache' => [
 			'class' => \yii\caching\FileCache::class,
 		],
