c8a97fc8dff6b8898dbcd7ea5d518682773a061e
p2made <pedrofp@me.com>
2024-12-03 13:51:52 +1000

    P2FreelancerModule

---
+++ p2-yii2/frontend/modules/P2FreelancerModule.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace frontend\modules;
+
+/**
+ * freelancer module definition class
+ */
+class P2FreelancerModule extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'frontend\modules\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
